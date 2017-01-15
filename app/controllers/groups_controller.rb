7a9545e00d3ed8f3bc3745bded2ee0a4db0f152b
zhangqian <zhq9877@gmail.com>
2017-01-15 21:47:07 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,10 +27,11 @@ class GroupsController < ApplicationController
 
   def update
     @group = Group.find(params[:id])
-
-    @group.update(groups_params)
-
-    redirect_to groups_path, notice:"Update success"
+    if @group.update(groups_params)
+      redirect_to groups_path, notice:"Update success"
+    else
+      render :edit
+    end
   end
 
   def destroy
